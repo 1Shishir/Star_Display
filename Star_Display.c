a963bd30bfba02692128f8f56db311c298db6949
Hasanur Rahman Shishir <90507563+1Shishir@users.noreply.github.com>
2022-03-13 20:59:05 +0600

    Add files via upload

---
+++ Star_Display.c
@@ -0,0 +1,205 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    int i;
+    printf("ENTER THE STRING IN CAPITAL\n");
+ab :
+    scanf("%c",&a);
+    for(i=1; i<=1; i++)
+    {
+        printf("\n\n\n");//LINE 1
+                for(int k=1; k<=1; k++)
+            {
+                if(a=='A' || a=='B' || a=='C' || a=='E' || a=='F' || a=='G' || a=='I' || a=='J' || a=='O'|| a=='P' || a=='R' || a=='S' || a=='T' || a=='Z')
+                {
+                    printf("\t*****");
+                }
+                else if(a=='H' || a=='M' || a=='N' || a=='U' || a=='V'|| a=='W' || a=='X' || a=='Y')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='K')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='D')
+                {
+                    printf("\t**** ");
+                }
+                else if(a=='L')
+                {
+                    printf("\t*");
+                }
+                else if(a=='Q')
+                {
+                    printf("\t****");
+                }
+                printf("\n");
+//LINE 2
+
+                if(a=='A' || a=='B' || a=='D' || a=='H' || a=='O' || a=='P' || a=='R' || a=='U' || a=='V' || a=='W')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='C' || a=='E' || a=='F' || a=='G' || a=='L'|| a=='S')
+                {
+                    printf("\t*    ");
+                }
+                else if(a=='I' || a=='T')
+                {
+                    printf("\t  *  ");
+                }
+                else if(a=='J')
+                {
+                    printf("\t    *");
+                }
+                else if(a=='K')
+                {
+                    printf("\t*  *  ");
+                }
+                else if(a=='N')
+                {
+                    printf("\t**  *");
+                }
+                else if(a=='Q')
+                {
+                    printf("\t*  *");
+                }
+                else if(a=='M')
+                {
+                    printf("\t** **");
+                }
+                else if(a=='X' || a=='Y')
+                {
+                    printf("\t * * ");
+                }
+                else if(a=='Z')
+                {
+                    printf("\t   * ");
+                }
+                printf("\n");
+//LINE 3
+                if(a=='A' || a=='E' || a=='F' || a=='P' || a=='R' || a=='H' || a=='S')
+                {
+                    printf("\t*****");
+                }
+                else if(a=='D' || a=='O' || a=='U' || a=='V')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='C'|| a=='L')
+                {
+                    printf("\t*");
+                }
+                else if(a=='G')
+                {
+                    printf("\t* ***");
+                }
+                else if(a=='B')
+                {
+                    printf("\t****");
+                }
+                else if(a=='X' || a=='I' || a=='T' || a=='Y' || a=='Z')
+                {
+                    printf("\t  *  ");
+                }
+                else if(a=='M' || a=='N' || a=='W')
+                {
+                    printf("\t* * *");
+                }
+                else if(a=='K')
+                {
+                    printf("\t***");
+                }
+                else if(a=='Q')
+                {
+                    printf("\t*  *");
+                }
+                else if(a=='J')
+                {
+                    printf("\t    *");
+                }
+                printf("\n");
+// LINE 4
+                if(a=='C' || a=='P' || a=='E' || a=='F' || a=='L')
+                {
+                    printf("\t*");
+                }
+                else if(a=='A' || a=='B' || a=='J' || a=='D' || a=='G' || a=='H'|| a=='M' || a=='O' || a=='U')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='I' || a=='T' || a=='Y')
+                {
+                    printf("\t  *  ");
+                }
+                else if(a=='S')
+                {
+                    printf("\t    *");
+                }
+                else if(a=='R')
+                {
+                    printf("\t* *");
+                }
+                else if(a=='K')
+                {
+                    printf("\t*  *");
+                }
+                else if(a=='N')
+                {
+                    printf("\t*  **");
+                }
+                else if(a=='Q')
+                {
+                    printf("\t****");
+                }
+                else if(a=='V' || a=='X')
+                {
+                    printf("\t * * ");
+                }
+                else if(a=='W')
+                {
+                    printf("\t** **");
+                }
+                else if(a=='Z')
+                {
+                    printf("\t *   ");
+                }
+                printf("\n");
+//LINE 5
+                if(a=='B' || a=='C' || a=='E' || a=='G' || a=='I' || a=='J' || a=='O' || a=='S' || a=='U'|| a=='Z' || a=='L')
+                {
+                    printf("\t*****");
+                }
+                else if(a=='H' || a=='A' || a=='N' || a=='M' || a=='R'|| a=='W' || a=='X')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='F' || a=='P')
+                {
+                    printf("\t*");
+                }
+                else if(a=='Q')
+                {
+                    printf("\t    *");
+                }
+                else if(a=='K')
+                {
+                    printf("\t*   *");
+                }
+                else if(a=='D')
+                {
+                    printf("\t****");
+                }
+                else if(a=='Y' || a=='T' || a=='V')
+                {
+                    printf("\t  *  ");
+                }
+                printf("\n");
+goto ab ;
+            }
+        }
+
+
+    }
